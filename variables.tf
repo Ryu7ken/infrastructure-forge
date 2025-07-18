f6e780865bcd99770a282e9112cf0b165f02bcd4
Ryu7ken <baquiur.rahman@outlook.com>
2025-07-18 12:38:08 +0530

    Removed Random Provider and Worked with Terraform Import

--- variables.tf
+++ variables.tf
@@ -6,3 +6,12 @@ variable "user_uuid" {
     error_message = "The user_uuid must be a valid UUID format (xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx)."
   }
 }
+
+variable "bucket_name" {
+  type        = string
+  description = "Name of the S3 bucket"
+  validation {
+    condition = can(regex("^[a-z0-9][a-z0-9.-]*[a-z0-9]$", var.bucket_name)) && length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && !can(regex("\\.\\.+", var.bucket_name)) && !can(regex("^[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}$", var.bucket_name))
+    error_message = "Bucket name must be 3-63 characters, start/end with lowercase letter or number, contain only lowercase letters, numbers, hyphens, and periods, not contain consecutive periods, and not be formatted as an IP address."
+  }
+}
