d229ee5afa57be92b85f638baf38c405b502f860
Ryu7ken <baquiur.rahman@outlook.com>
2025-08-09 12:17:56 +0530

    Implemented Terraform Provider Block in main.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
-output "bucket_name" {
-    description = "Bucket name for our static website hosting"
-    value = module.terrahouse.bucket_name
-}
+# output "bucket_name" {
+#     description = "Bucket name for our static website hosting"
+#     value = module.terrahouse.bucket_name
+# }
 
-output "s3_website_endpoint" {
-    description = "S3 Static Website Hosting Endpoint"
-    value = module.terrahouse.website_endpoint
-}
+# output "s3_website_endpoint" {
+#     description = "S3 Static Website Hosting Endpoint"
+#     value = module.terrahouse.website_endpoint
+# }
 
-output "cloudfront_endpoint" {
-    description = "CloudFront Distribution URL"
-    value = module.terrahouse.cloudfront_domain
-}
+# output "cloudfront_endpoint" {
+#     description = "CloudFront Distribution URL"
+#     value = module.terrahouse.cloudfront_domain
+# }
